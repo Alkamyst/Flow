ce12f6f9f0706061a9c91058b25dc51518aa541f
Logan Conrad <loganmconrad@gmail.com>
2026-03-15 16:59:28 -0400

    images

--- Global/globals.gd
+++ Global/globals.gd
@@ -8,3 +8,8 @@ var cur_level = 0
 var reset: bool = false
 var next_level: bool = false
 var paused: bool = false
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Fullscreen"):
+		var mode := DisplayServer.window_get_mode()
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if mode != DisplayServer.WINDOW_MODE_FULLSCREEN else DisplayServer.WINDOW_MODE_WINDOWED)
